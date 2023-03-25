aa30846e0bc59af13458c77a2cf710abb8d7c022
@wenxihuw <wenxihuw@gmail.com>
2023-03-25 12:47:50 -0400

    Edited

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,3 +1,34 @@
 #!/usr/bin/env node
 
 import { rps } from '../lib/rpsls.js';
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+
+if (args.h || args.help) {
+    console.log(
+        `Usage: node-rps [SHOT]
+        Play Rock Paper Scissors (RPS)
+        
+          -h, --help      display this help message and exit
+          -r, --rules     display the rules and exit
+        
+        Examples:
+          node-rps        Return JSON with single player RPS result.
+                          e.g. {"player":"rock"}
+          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                          e.g {"player":"rock","opponent":"scissors","result":"win"}`
+    );
+    process.exit(0);
+}
+
+if (args.r || args.rules) {
+    console.log(
+        `Rules for Rock Paper Scissors:
+
+        - Scissors CUTS Paper
+        - Paper COVERS Rock
+        - Rock CRUSHES Scissors`
+    );
+    process.exit(0);
+}
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
 import { rpsls } from "../lib/rpsls.js";
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
