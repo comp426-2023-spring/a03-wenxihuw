8d196dffab946a67bef1cf76d8546a393f102dfe
@wenxihuw <wenxihuw@gmail.com>
2023-03-25 16:50:59 -0400

    test

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -59,6 +59,6 @@ if (args._.length > 1) {
     process.exit(0);
 // }else if (args._.length === 0) {
 //   console.log(JSON.stringify(rps()))
-} else {
-  console.log(JSON.stringify(rps(...args._.slice(2))))
+// } else {
+//   console.log(JSON.stringify(rps(...args._.slice(2))))
 }
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -68,8 +68,8 @@ if (args._.length > 3) {
         `
     );
     process.exit(0);
-} else if (args._.length === 0) {
-    console.log(JSON.stringify(rpsls()))
-} else {
-    console.log(JSON.stringify(rpsls(...args._.slice(2))))
+// } else if (args._.length === 0) {
+//     console.log(JSON.stringify(rpsls()))
+// } else {
+//     console.log(JSON.stringify(rpsls(...args._.slice(2))))
 }
